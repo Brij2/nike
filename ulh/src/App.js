0767abb53458bafe2f919ab6aa221cb5e248f917
brij2 <brij4268@gmail.com>
2022-07-29 13:22:10 +0530

    first comit

---
+++ ulh/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar title="Brijesh_kumar" />
+
+    </>
+  );
+}
+
+export default App;
